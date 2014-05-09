3060ae9055c74a6faf7c38e409a393387adfc151
Ryan Hall <ryan.hall@biola.edu>
2014-05-09 12:12:27 -0700

    initial commit

---
+++ onload.js
@@ -0,0 +1,148 @@
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+
+// This file contains several workarounds on inconsistent browser behaviors that administrators may customize.
+"use strict";
+
+// iPhone email friendly keyboard does not include "\" key, use regular keyboard instead.
+// Note change input type does not work on all versions of all browsers.
+if (navigator.userAgent.match(/iPhone/i) != null) {
+    var emails = document.querySelectorAll("input[type='email']");
+    if (emails) {
+        for (var i = 0; i < emails.length; i++) {
+            emails[i].type = 'text';
+        }
+    }
+}
+
+// In the CSS file we set the ms-viewport to be consistent with the device dimensions,
+// which is necessary for correct functionality of immersive IE.
+// However, for Windows 8 phone we need to reset the ms-viewport's dimension to its original
+// values (auto), otherwise the viewport dimensions will be wrong for Windows 8 phone.
+// Windows 8 phone has agent string 'IEMobile 10.0'
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement("style");
+    msViewportStyle.appendChild(
+        document.createTextNode(
+            "@-ms-viewport{width:auto!important}"
+        )
+    );
+    msViewportStyle.appendChild(
+        document.createTextNode(
+            "@-ms-viewport{height:auto!important}"
+        )
+    );
+    document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
+}
+
+// If the innerWidth is defined, use it as the viewport width.
+if (window.innerWidth && window.outerWidth && window.innerWidth !== window.outerWidth) {
+    var viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute('content', 'width=' + window.innerWidth + 'px; initial-scale=1.0; maximum-scale=1.0');
+}
+
+// Gets the current style of a specific property for a specific element.
+function getStyle(element, styleProp) {
+    var propStyle = null;
+
+    if (element && element.currentStyle) {
+        propStyle = element.currentStyle[styleProp];
+    }
+    else if (element && window.getComputedStyle) {
+        propStyle = document.defaultView.getComputedStyle(element, null).getPropertyValue(styleProp);
+    }
+
+    return propStyle;
+}
+
+// The script below is used for downloading the illustration image
+// only when the branding is displaying. This script work together
+// with the code in PageBase.cs that sets the html inline style
+// containing the class 'illustrationClass' with the background image.
+var computeLoadIllustration = function () {
+    var branding = document.getElementById("branding");
+    var brandingDisplay = getStyle(branding, "display");
+    var brandingWrapperDisplay = getStyle(document.getElementById("brandingWrapper"), "display");
+
+    if (brandingDisplay && brandingDisplay !== "none" &&
+        brandingWrapperDisplay && brandingWrapperDisplay !== "none") {
+        var newClass = "illustrationClass";
+
+        if (branding.classList && branding.classList.add) {
+            branding.classList.add(newClass);
+        } else if (branding.className !== undefined) {
+            branding.className += " " + newClass;
+        }
+        if (window.removeEventListener) {
+            window.removeEventListener('load', computeLoadIllustration, false);
+            window.removeEventListener('resize', computeLoadIllustration, false);
+        }
+        else if (window.detachEvent) {
+            window.detachEvent('onload', computeLoadIllustration);
+            window.detachEvent('onresize', computeLoadIllustration);
+        }
+    }
+};
+
+if (window.addEventListener) {
+    window.addEventListener('resize', computeLoadIllustration, false);
+    window.addEventListener('load', computeLoadIllustration, false);
+}
+else if (window.attachEvent) {
+    window.attachEvent('onresize', computeLoadIllustration);
+    window.attachEvent('onload', computeLoadIllustration);
+}
+
+
+
+
+/************************************/
+/******   Custom Javascript   *******/
+/************************************/
+
+
+// This hasn't been tested yet
+// // Append the domain to the username if it's not there.
+// var AppendUPN = function () {
+//   var userName = document.getElementById(Login.userNameInput);
+//   if (userName.value) {
+//     var lowerUserName = userName.value.toLowerCase();
+//     var li = lowerUserName.lastIndexOf('@biola');
+//     if (li == -1) {
+//       userName.value = userName.value + '@biola';
+//     }
+//     return true;
+//   }
+//   return false;
+// }
+
+
+var userNameInput = document.getElementById("userNameInput");
+if (userNameInput) {
+
+  // Replace the default placeholder in the username input
+  userNameInput.placeholder = 'biola\\username';
+
+
+  // This hasn't been tested yet
+  // // Check for domain in username on submit
+  // document.getElementById('submitButton').onclick = function() {
+  //   if (AppendUPN()) {
+  //     return Login.submitLoginRequest();
+  //   }
+  // }
+  // document.getElementById('submitButton').onkeypress = function() {
+  //   if (event && event.keyCode == 13) {
+  //     if (AppendUPN()) {
+  //       Login.submitLoginRequest();
+  //     }
+  //   }
+  // }
+  // document.getElementById('loginForm').onkeypress = function() {
+  //   if (event && event.keyCode == 13) {
+  //     if (AppendUPN()) {
+  //       Login.submitLoginRequest();
+  //     }
+  //   }
+  // }
+
+}
---
+++ readme.md
@@ -0,0 +1,57 @@
+## For reference
+
+* http://technet.microsoft.com/en-us/library/dn280950.aspx
+* http://technet.microsoft.com/en-us/library/dn636121.aspx
+
+## Customizing the SAML login screen
+
+Copy the current directory to `c:\adfs_theme`
+
+Copy default theme to a custom theme
+
+    New-AdfsWebTheme -Name custom -SourceName default
+
+Activate the custom theme
+
+    Set-AdfsWebConfig -ActiveThemeName custom
+
+Set company name
+
+    Set-AdfsGlobalWebContent –CompanyName "Biola University"
+
+Set logo and background
+
+    Set-AdfsWebTheme -TargetName custom -Illustration @{path="c:\adfs_theme\illustration.jpg"}
+    Set-AdfsWebTheme -TargetName custom -Logo @{path="c:\adfs_theme\logo.png"}
+
+Add Home Link (Not needed)
+
+    Set-AdfsGlobalWebContent -HomeLink http://www.biola.edu/ -HomeLinkText Home
+
+Add Helpdesk link
+
+    Set-AdfsGlobalWebContent -HelpDeskLink http://offices1.biola.edu/it/contact/ -HelpDeskLinkText Help
+
+Add Privacy link
+
+    Set-AdfsGlobalWebContent -PrivacyLink http://www.biola.edu/privacy/ -PrivacyLinkText Privacy
+
+Add custom error messages *(I'm not sure what these should actually say, the default messages might be fine for now)*
+
+    Set-AdfsGlobalWebContent -ErrorPageDescriptionText "An error occurred"
+    Set-AdfsGlobalWebContent -ErrorPageGenericErrorMessage "An error occurred. Contact the IT helpdesk for more information."
+    Set-AdfsGlobalWebContent -ErrorPageAuthorizationErrorMessage "You have received an Authorization error. Contact the IT helpdesk for assistance."
+    Set-AdfsGlobalWebContent -ErrorPageDeviceAuthenticationErrorMessage "Your device is not authorized. Contact the IT helpdesk for assistance."
+    Set-AdfsGlobalWebContent -ErrorPageSupportEmail  "it.helpdesk@biola.edu"
+
+## Optional steps
+*(these don't need to be run, they are just here for reference)*
+
+Export custom theme
+
+    Export-AdfsWebTheme -Name custom -Directory c:\adfs_theme
+
+Sign in page description (not needed)
+
+    Set-AdfsGlobalWebContent -SignInPageDescriptionText "<p>Sign-in to Contoso requires device registration. Click <A href='http://fs1.contoso.com/deviceregistration/'>here</A> for more information.</p>"
+
